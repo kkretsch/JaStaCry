0637e089fc8c41b7a8685999631facdd8e061923
Kai KRETSCHMANN <kai@kretschmann.consulting>
2017-10-19 12:20:01 +0200

    remove source style warnings

--- src/main/java/org/jastacry/layer/RotateLayer.java
+++ src/main/java/org/jastacry/layer/RotateLayer.java
@@ -6,52 +6,56 @@ import java.io.OutputStream;
 
 /**
  * Rotate every byte by an offset (either positiv or negativ).
+ * 
  * @author Kai Kretschmann
  * @version 0.1.20130818
  */
 
 public class RotateLayer extends AbsLayer {
-	public final static String LAYERNAME="Rotate Layer";
-	private int iOffset;
+    public final static String LAYERNAME = "Rotate Layer";
+    private int iOffset;
 
-	@Override
-	public void init(String data) {
-		this.iOffset = Integer.parseInt(data);
-	}
+    @Override
+    public void init(final String data) {
+        this.iOffset = Integer.parseInt(data);
+    }
 
-	@Override
-	public void encStream(InputStream is, OutputStream os) throws IOException {
-		int iChar;
-		while((iChar = is.read()) != -1) {
-			iChar += this.iOffset;
-			rangeCheck(iChar);
-			os.write(iChar);
-		}
-	}
+    @Override
+    public void encStream(final InputStream is, final OutputStream os) throws IOException {
+        int iChar;
+        while ((iChar = is.read()) != -1) {
+            iChar += this.iOffset;
+            rangeCheck(iChar);
+            os.write(iChar);
+        }
+    }
 
-	@Override
-	public void decStream(InputStream is, OutputStream os) throws IOException {
-		int iChar;
-		while((iChar = is.read()) != -1) {
-			iChar -= this.iOffset;
-			rangeCheck(iChar);
-			os.write(iChar);
-		}
-	}
+    @Override
+    public void decStream(final InputStream is, final OutputStream os) throws IOException {
+        int iChar;
+        while ((iChar = is.read()) != -1) {
+            iChar -= this.iOffset;
+            rangeCheck(iChar);
+            os.write(iChar);
+        }
+    }
 
-	@Override
-	public String toString() {
-		return LAYERNAME;
-	}
+    @Override
+    public String toString() {
+        return LAYERNAME;
+    }
 
-	private int rangeCheck(int i) {
-		if(i<0) {
-			i+= 256;
-		} else {
-			if(i>255) i-=256;
-		}
+    private int rangeCheck(final int i) {
+        int iTmp = i;
+        if (iTmp < 0) {
+            iTmp += 256;
+        } else {
+            if (iTmp > 255) {
+                iTmp -= 256;
+            }
+        }
 
-		return i;
-	}
-	
+        return iTmp;
+    }
+
 }
