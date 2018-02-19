9b640b21e5fac4749c51b9569d2990dfdc5023b8
kkretsch <kai@kaikretschmann.de>
2018-02-19 08:59:25 +0100

    add coverage of IOException.

---
+++ src/test/java/org/jastacry/test/TestIO.java
@@ -0,0 +1,84 @@
+package org.jastacry.test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.concurrent.CountDownLatch;
+
+import org.jastacry.layer.ReadWriteLayer;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+/**
+ * Test of IOException.
+ *
+ * @author Kai Kretschmann
+ *
+ */
+public class TestIO {
+    /**
+     * Testdata to play with.
+     */
+    private final String testdata = "The quick brown fox jumps over the lazy dog.";
+
+    /**
+     * The layer to test.
+     */
+    private ReadWriteLayer layer = null;
+
+    private CountDownLatch endController;
+
+    /**
+     * Test before method.
+     *
+     * @throws Exception
+     *             in case of error
+     */
+    @Before
+    public void setUp() throws Exception {
+        layer = new ReadWriteLayer();
+        endController = new CountDownLatch(1);
+        layer.setEndController(endController);
+    }
+
+    /**
+     * Test After method.
+     *
+     * @throws Exception
+     *             in case of error
+     */
+    @After
+    public void tearDown() throws Exception {
+        layer = null;
+    }
+
+    /**
+     * Testcase testIOException.
+     *
+     */
+    @Test
+    public void testIOException() {
+        InputStream sInput;
+        OutputStream sOutput;
+        sInput = new InputStream() {
+            @Override
+            public int read() throws IOException {
+                throw new IOException("Expected as a test");
+            }
+        };
+        sOutput = new OutputStream() {
+            @Override
+            public void write(int i) throws IOException {
+                throw new IOException("Expected as a test");
+            }
+        };
+
+        layer.setInputStream(sInput);
+        layer.setOutputStream(sOutput);
+        layer.run();
+    }
+
+}
