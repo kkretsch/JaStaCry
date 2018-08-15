79141e7dd3941253bc6365db7b4a2880ff80c895
kkretsch <kai@kaikretschmann.de>
2018-08-15 17:15:05 +0200

    Update naming for TestIO.

--- src/test/java/org/jastacry/test/AllTests.java
+++ src/test/java/org/jastacry/test/AllTests.java
@@ -16,7 +16,7 @@ import org.junit.runners.Suite.SuiteClasses;
 @SuiteClasses({ TestExportEncryption.class, TestConfig.class, TestEntropy.class, TestLayerAesCbc.class,
         TestLayerAesEcb.class, TestLayerAsciiTransport.class, TestLayerFilemerge.class, TestLayerMd5Des.class,
         TestLayerRandom.class, TestLayerReverse.class, TestLayerRotate.class, TestLayerTransparent.class,
-        TestLayerXor.class, TestIO.class, TestMain.class })
+        TestLayerXor.class, TestInputOutput.class, TestMain.class })
 public class AllTests
 {
 
