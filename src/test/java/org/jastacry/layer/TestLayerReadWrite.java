5cb1385c091c4b2038cfd38c9ea11c4382d93c04
kkretsch <kai@kaikretschmann.de>
2018-08-29 08:53:06 +0200

    Add new test class for LayerReadWrite

---
+++ src/test/java/org/jastacry/layer/TestLayerReadWrite.java
@@ -0,0 +1,81 @@
+package org.jastacry.layer;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.jastacry.JastacryException;
+import org.jastacry.layer.RotateLayer;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test of Rotate Layer.
+ *
+ * @author Kai Kretschmann
+ *
+ */
+public class TestLayerReadWrite
+{
+    /**
+     * Testdata to play with.
+     */
+    private final String testdata = "The quick brown fox jumps over the lazy dog.";
+
+    /**
+     * The layer to test.
+     */
+    private ReadWriteLayer layer = null;
+
+    /**
+     * Test Before method.
+     *
+     * @throws Exception
+     *             in case of error
+     */
+    @Before
+    public void setUp() throws Exception
+    {
+        layer = new ReadWriteLayer();
+    }
+
+    /**
+     * Test After method.
+     *
+     * @throws Exception
+     *             in case of error
+     */
+    @After
+    public void tearDown() throws Exception
+    {
+        layer = null;
+    }
+
+    /**
+     * Testcase testToString.
+     */
+    @Test
+    public void testToString()
+    {
+        assertEquals("Layer name mismatch", ReadWriteLayer.LAYERNAME, layer.toString());
+    }
+
+    /**
+     * Testcase unsupported exception.
+     * @throws JastacryException on error
+     */
+    @Test(expected = UnsupportedOperationException.class)
+    public void testUnsupported() throws JastacryException
+    {
+        byte[] buf = testdata.getBytes();
+        final InputStream isEncode = new ByteArrayInputStream(buf);
+        final ByteArrayOutputStream osEncode = new ByteArrayOutputStream();
+        layer.encodeAndDecode(isEncode, osEncode);
+    }
+
+}
