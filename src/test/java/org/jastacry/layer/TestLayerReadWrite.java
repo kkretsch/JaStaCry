b921ca99a271e0fe381bfb8960a52c4c8072fcbe
Kai Kretschmann <kai@kaikretschmann.de>
2018-10-10 08:23:40 +0200

    Add unit tests to ReadWrite layer.
    JAS-13

--- src/test/java/org/jastacry/layer/TestLayerReadWrite.java
+++ src/test/java/org/jastacry/layer/TestLayerReadWrite.java
@@ -4,13 +4,9 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 
 import org.jastacry.JastacryException;
-import org.jastacry.layer.RotateLayer;
-import org.jastacry.test.utils.Tooling;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -86,6 +82,60 @@ public class TestLayerReadWrite
     public void testInit()
     {
         layer.init("");
+    }
+
+    /**
+     * Testcase equals.
+     */
+    @Test
+    public void testEquals()
+    {
+        ReadWriteLayer l1 = new ReadWriteLayer();
+        ReadWriteLayer l2 = new ReadWriteLayer();
+        assertEquals("Layer object equal", l1, l2);
+    }
+
+    /**
+     * Testcase equals.
+     */
+    @Test
+    public void testEqualsSame()
+    {
+        ReadWriteLayer l1 = new ReadWriteLayer();
+        assertEquals("Layer object same", l1, l1);
+    }
+
+    /**
+     * Testcase equals.
+     */
+    @Test
+    public void testNotEqualsNull()
+    {
+        ReadWriteLayer l1 = new ReadWriteLayer();
+        Object o = null;
+        assertEquals("Layer object null unequal", l1.equals(o), false);
+    }
+
+    /**
+     * Testcase equals.
+     */
+    @Test
+    public void testNotEqualsWrongclass()
+    {
+        ReadWriteLayer l1 = new ReadWriteLayer();
+        Object o = new Object();
+        assertEquals("Layer object wrong class unequal", l1.equals(o), false);
+    }
+
+    /**
+     * Testcase hashcode.
+     */
+    @Test
+    public void testHashcode()
+    {
+        ReadWriteLayer l1 = new ReadWriteLayer();
+        ReadWriteLayer l2 = new ReadWriteLayer();
+        assertEquals("Layer hash equal", l1.hashCode(), l2.hashCode());
     }
 
 }
