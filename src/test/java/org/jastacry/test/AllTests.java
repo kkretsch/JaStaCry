16feb7defc0657374c719b4e92f457b363a11bfe
Kai Kretschmann <kai@kaikretschmann.de>
2018-11-27 08:45:28 +0100

    add AbstractLayer test class

--- src/test/java/org/jastacry/test/AllTests.java
+++ src/test/java/org/jastacry/test/AllTests.java
@@ -12,6 +12,7 @@ import org.jastacry.layer.TestLayerReverse;
 import org.jastacry.layer.TestLayerRotate;
 import org.jastacry.layer.TestLayerTransparent;
 import org.jastacry.layer.TestLayerXor;
+import org.jastacry.layer.TestAbstractBasicLayer;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
@@ -26,7 +27,8 @@ import org.junit.runners.Suite.SuiteClasses;
 @SuiteClasses({
     TestExportEncryption.class, TestConfig.class, TestEntropy.class, TestLayerAesCbc.class, TestLayerAesEcb.class,
     TestLayerAsciiTransport.class, TestLayerFilemerge.class, TestLayerMd5Des.class, TestLayerRandom.class, TestLayerReverse.class,
-    TestLayerRotate.class, TestLayerTransparent.class, TestLayerXor.class, TestInputOutput.class, TestMain.class, TestLocale.class
+    TestLayerRotate.class, TestLayerTransparent.class, TestLayerXor.class, TestInputOutput.class, TestMain.class, TestLocale.class,
+    TestAbstractBasicLayer.class
 })
 public class AllTests
 {
