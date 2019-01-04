76049dfbe5f5bab67efc203170836bfe2ef71e87
Kai Kretschmann <kai@kaikretschmann.de>
2019-01-04 16:47:15 +0100

    small fixes

--- src/test/java/org/jastacry/layer/TestAbstractBasicLayer.java
+++ src/test/java/org/jastacry/layer/TestAbstractBasicLayer.java
@@ -2,13 +2,11 @@ package org.jastacry.layer;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.concurrent.CountDownLatch;
 
 import org.jastacry.GlobalData.Action;
-import org.jastacry.JastacryException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
