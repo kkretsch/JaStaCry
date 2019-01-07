a675f4a284bee1e6269299aa759138a1add0ff35
Kai Kretschmann <kai@kaikretschmann.de>
2019-01-07 10:38:55 +0100

    More coverage on GlobalFunctions

--- src/test/java/org/jastacry/test/AllTests.java
+++ src/test/java/org/jastacry/test/AllTests.java
@@ -28,7 +28,7 @@ import org.junit.runners.Suite.SuiteClasses;
     TestExportEncryption.class, TestConfig.class, TestEntropy.class, TestLayerAesCbc.class, TestLayerAesEcb.class,
     TestLayerAsciiTransport.class, TestLayerFilemerge.class, TestLayerMd5Des.class, TestLayerRandom.class, TestLayerReverse.class,
     TestLayerRotate.class, TestLayerTransparent.class, TestLayerXor.class, TestInputOutput.class, TestMain.class, TestLocale.class,
-    TestAbstractBasicLayer.class
+    TestAbstractBasicLayer.class, TestGlobalFunctions.class
 })
 public class AllTests
 {
