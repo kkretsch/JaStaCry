4fc5cad6d1303d8214218842e2d9ea9a4f2847f1
Kai Kretschmann <kai@kaikretschmann.de>
2019-01-15 15:32:07 +0100

    Update testsuite to JUnit5 version.
    JAS-17

--- src/test/java/org/jastacry/test/AllTests.java
+++ src/test/java/org/jastacry/test/AllTests.java
@@ -1,7 +1,6 @@
 package org.jastacry.test;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.jastacry.layer.TestAbstractBasicLayer;
 import org.jastacry.layer.TestLayerAesCbc;
 import org.jastacry.layer.TestLayerAesEcb;
 import org.jastacry.layer.TestLayerAsciiTransport;
@@ -12,10 +11,11 @@ import org.jastacry.layer.TestLayerReverse;
 import org.jastacry.layer.TestLayerRotate;
 import org.jastacry.layer.TestLayerTransparent;
 import org.jastacry.layer.TestLayerXor;
-import org.jastacry.layer.TestAbstractBasicLayer;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectClasses;
 import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
+
 
 /**
  * All test cases in one suite.
@@ -23,8 +23,8 @@ import org.junit.runners.Suite.SuiteClasses;
  * @author Kai Kretschmann
  *
  */
-@RunWith(Suite.class)
-@SuiteClasses({
+@RunWith(JUnitPlatform.class)
+@SelectClasses({
     TestExportEncryption.class, TestConfig.class, TestEntropy.class, TestLayerAesCbc.class, TestLayerAesEcb.class,
     TestLayerAsciiTransport.class, TestLayerFilemerge.class, TestLayerMd5Des.class, TestLayerRandom.class, TestLayerReverse.class,
     TestLayerRotate.class, TestLayerTransparent.class, TestLayerXor.class, TestInputOutput.class, TestMain.class, TestLocale.class,
@@ -32,18 +32,4 @@ import org.junit.runners.Suite.SuiteClasses;
 })
 public class AllTests
 {
-
-    /**
-     * log4j logger object.
-     */
-    private static final Logger LOGGER = LogManager.getLogger(AllTests.class.getName());
-
-    /**
-     * All tests, initial test suite starting point.
-     */
-    public AllTests()
-    {
-        // empty constructor
-        LOGGER.info("running AllTests");
-    }
 }
