b3f33b032a16dbd616c8315843f54ffc75d68fdb
Kai Kretschmann <kai@kaikretschmann.de>
2019-01-15 15:36:53 +0100

    Add complete set of test cases to testsuite.
    JAS-17

--- src/test/java/org/jastacry/test/AllTests.java
+++ src/test/java/org/jastacry/test/AllTests.java
@@ -2,16 +2,17 @@ package org.jastacry.test;
 
 import org.jastacry.layer.TestAbstractBasicLayer;
 import org.jastacry.layer.TestLayerAesCbc;
+import org.jastacry.layer.TestLayerAesCtr;
 import org.jastacry.layer.TestLayerAesEcb;
 import org.jastacry.layer.TestLayerAsciiTransport;
 import org.jastacry.layer.TestLayerFilemerge;
 import org.jastacry.layer.TestLayerMd5Des;
 import org.jastacry.layer.TestLayerRandom;
+import org.jastacry.layer.TestLayerReadWrite;
 import org.jastacry.layer.TestLayerReverse;
 import org.jastacry.layer.TestLayerRotate;
 import org.jastacry.layer.TestLayerTransparent;
 import org.jastacry.layer.TestLayerXor;
-
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.runner.RunWith;
@@ -25,10 +26,11 @@ import org.junit.runner.RunWith;
  */
 @RunWith(JUnitPlatform.class)
 @SelectClasses({
-    TestExportEncryption.class, TestConfig.class, TestEntropy.class, TestLayerAesCbc.class, TestLayerAesEcb.class,
-    TestLayerAsciiTransport.class, TestLayerFilemerge.class, TestLayerMd5Des.class, TestLayerRandom.class, TestLayerReverse.class,
-    TestLayerRotate.class, TestLayerTransparent.class, TestLayerXor.class, TestInputOutput.class, TestMain.class, TestLocale.class,
-    TestAbstractBasicLayer.class, TestGlobalFunctions.class
+    TestConfig.class, TestEntropy.class, TestExportEncryption.class, TestGlobalFunctions.class, TestInputOutput.class, TestLocale.class, TestMain.class,
+    TestAbstractBasicLayer.class,
+    TestLayerAesCbc.class, TestLayerAesCtr.class, TestLayerAesEcb.class,
+    TestLayerAsciiTransport.class, TestLayerFilemerge.class, TestLayerMd5Des.class, TestLayerRandom.class, TestLayerReadWrite.class, TestLayerReverse.class,
+    TestLayerRotate.class, TestLayerTransparent.class, TestLayerXor.class
 })
 public class AllTests
 {
