d350467c8929a8796a1a75ee96e844ceed3cceab
Kai Kretschmann <kai@kaikretschmann.de>
2019-01-22 15:31:14 +0100

    Move console into interface for better testing mockup

---
+++ src/main/java/org/jastacry/ConsoleServiceInterface.java
@@ -0,0 +1,11 @@
+package org.jastacry;
+
+/**
+ * Interface around console.
+ * @author Kai Kretschmann
+ *
+ */
+public interface ConsoleServiceInterface
+{
+    public char[] readPassword(final String prompt);
+}
