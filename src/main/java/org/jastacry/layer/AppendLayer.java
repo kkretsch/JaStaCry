f9385f22e104997c49806905b7be34ce348e6172
Kai KRETSCHMANN <kai@kretschmann.consulting>
2019-10-03 16:25:11 +0200

    Add new layer AppendLayer
    JAS-22 simple implementation of file appending

---
+++ src/main/java/org/jastacry/layer/AppendLayer.java
@@ -0,0 +1,170 @@
+package org.jastacry.layer;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Objects;
+
+import org.jastacry.JastacryException;
+
+/**
+ * Append the bytes from a file to the current byte stream.
+ * On decryption just strip it from the byte stream.
+ *
+ * <p>SPDX-License-Identifier: MIT
+ *
+ * @author Kai Kretschmann
+ */
+public class AppendLayer extends AbstractBasicLayer
+{
+    /**
+     * static name of the layer.
+     */
+    public static final String LAYERNAME = "Append Layer";
+
+    /**
+     * File to append after data.
+     */
+    private File fileAppend;
+
+    /**
+     * Constructor of FilemergeLayer.
+     */
+    public AppendLayer()
+    {
+        super(AppendLayer.class, LAYERNAME);
+        logger.debug("create layer class");
+        this.fileAppend = null;
+    }
+
+    /**
+     * init function.
+     *
+     * @param data to initialise the file.
+     */
+    @SuppressWarnings("squid:S4797") // Handling files is security-sensitive
+    @Override
+    public final void init(final String data)
+    {
+        logger.info("use file '{}' for appending", data);
+        this.fileAppend = new File(data);
+    }
+
+    /**
+     * Encode data.
+     */
+    @Override
+    public void encStream(InputStream inputStream, OutputStream outputStream) throws JastacryException
+    {
+        if(null == fileAppend)
+        {
+            throw (JastacryException) new JastacryException("encodeAndDecode failed, append file undefined");
+        }
+
+        try(FileInputStream fIS = new FileInputStream(fileAppend))
+        {
+            logger.debug("read data input stream");
+            int iChar;
+            while ((iChar = inputStream.read()) != -1)
+            {
+                outputStream.write(iChar);
+            }
+            logger.debug("read append file stream");
+            while ((iChar = fIS.read()) != -1)
+            {
+                outputStream.write(iChar);
+            }
+
+            logger.debug("close pipe");
+            outputStream.close();
+        }
+        catch (IOException e)
+        {
+            throw (JastacryException) new JastacryException("encodeAndDecode failed").initCause(e);
+        }
+    }
+
+    /**
+     * Decode back.
+     */
+    @Override
+    public void decStream(InputStream inputStream, OutputStream outputStream) throws JastacryException
+    {
+        if(null == fileAppend)
+        {
+            throw (JastacryException) new JastacryException("encodeAndDecode failed, append file undefined");
+        }
+        long lFileSize = fileAppend.length();
+
+        try
+        {
+            if(0 == inputStream.available())
+            {
+                throw (JastacryException) new JastacryException("encodeAndDecode failed, input stream empty");
+            }
+
+            byte[] buf = inputStream.readAllBytes();
+            if(buf.length < lFileSize)
+            {
+                throw (JastacryException) new JastacryException("decStream failed, too short");
+            }
+            int lDataSize = (int) (buf.length-lFileSize);
+            byte[] bufData = new byte[lDataSize];
+            java.lang.System.arraycopy(buf, 0, bufData, 0, lDataSize);
+            outputStream.write(bufData);
+
+            logger.debug("close pipe");
+            outputStream.close();
+        }
+        catch (IOException e)
+        {
+            throw (JastacryException) new JastacryException("decStream failed").initCause(e);
+        }
+    }
+
+    /**
+     * merge Stream function.
+     *
+     * @param inputStream input stream
+     * @param outputStream output stream
+     * @throws JastacryException in case of error
+     */
+    @Override
+    public final void encodeAndDecode(final InputStream inputStream, final OutputStream outputStream) throws JastacryException
+    {
+        throw new RuntimeException("Combined method not to be used here.");
+    }
+
+    /**
+     * Override equals method from object class.
+     * @param o object to compare with
+     * @return true or false
+     */
+    @Override
+    public boolean equals(final Object o)
+    {
+        if (o == this)
+        {
+            return true;
+        }
+        if (!(o instanceof AppendLayer))
+        {
+            return false;
+        }
+
+        final AppendLayer layer = (AppendLayer) o;
+        return layer.fileAppend.equals(this.fileAppend);
+    }
+
+    /**
+     * Override equals method from object class.
+     * @return hash of properties
+     */
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(fileAppend);
+    }
+}
---
+++ src/test/java/org/jastacry/layer/TestLayerAppend.java
@@ -0,0 +1,231 @@
+package org.jastacry.layer;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.jastacry.JastacryException;
+import org.jastacry.test.utils.Tooling;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * Test of Layer Random.
+ *
+ * @author Kai Kretschmann
+ *
+ */
+public class TestLayerAppend
+{
+    /**
+     * Test data to play with.
+     */
+    private final String testdata = "The quick brown fox jumps over the lazy dog.";
+
+    /**
+     * The layer to test.
+     */
+    private AppendLayer layer = null;
+
+    /**
+     * Init value for random layer.
+     */
+    private static final String INITVALUE = "src/test/resources/foto.jpg";
+
+    /**
+     * Init value for random layer.
+     */
+    private static final String LONGTEXTFILE = "src/test/resources/longtext.txt";
+
+    /**
+     * Test Before method.
+     *
+     * @throws Exception
+     *             in case of error
+     */
+    @BeforeEach
+    public void setUp() throws Exception
+    {
+        layer = new AppendLayer();
+    }
+
+    /**
+     * Test After method.
+     *
+     * @throws Exception
+     *             in case of error
+     */
+    @AfterEach
+    public void tearDown() throws Exception
+    {
+        layer = null;
+    }
+
+    /**
+     * Testcase testEncDecStream.
+     *
+     * @throws JastacryException
+     *             in case of error
+     */
+    @Test
+    public void testEncDecStream() throws JastacryException
+    {
+        byte[] buf = testdata.getBytes();
+        final InputStream isEncode = new ByteArrayInputStream(buf);
+        final ByteArrayOutputStream osEncode = new ByteArrayOutputStream();
+        layer.init(INITVALUE);
+        layer.encStream(isEncode, osEncode);
+        buf = osEncode.toByteArray();
+
+        layer = null;
+        layer = new AppendLayer();
+        final InputStream isDecode = new ByteArrayInputStream(buf);
+        final OutputStream osDecode = new ByteArrayOutputStream();
+        layer.init(INITVALUE);
+        layer.decStream(isDecode, osDecode);
+        assertEquals("decoding differs", testdata, osDecode.toString());
+    }
+
+    /**
+     * Testcase testEncDecStream.
+     *
+     * @throws JastacryException
+     *             in case of error
+     * @throws IOException in case of error
+     */
+    @Test
+    // TestLink(externalId = "JAS-6")
+    public void testEncDecStreamLong() throws JastacryException, IOException
+    {
+        InputStream is = new FileInputStream(LONGTEXTFILE);
+        byte[] buf = IOUtils.toByteArray(is);
+
+        final InputStream isEncode = new ByteArrayInputStream(buf);
+        final ByteArrayOutputStream osEncode = new ByteArrayOutputStream();
+        layer.init(INITVALUE);
+        layer.encStream(isEncode, osEncode);
+        byte[] buf2 = osEncode.toByteArray();
+
+        layer = null;
+        layer = new AppendLayer();
+        final InputStream isDecode = new ByteArrayInputStream(buf2);
+        final OutputStream osDecode = new ByteArrayOutputStream();
+        layer.init(INITVALUE);
+        layer.decStream(isDecode, osDecode);
+
+        String sTextcontent = new String(buf, "ISO-8859-1");
+        assertEquals("decoding differs", sTextcontent, osDecode.toString());
+    }
+
+    /**
+     * Testcase testEncStream Exceptions.
+     *
+     * @throws JastacryException
+     *             in case of error
+     * @throws IOException will be thrown in test
+     */
+    @Test
+    public void testEncStreamException() throws JastacryException, IOException
+    {
+        Tooling tool = new Tooling();
+        Assertions.assertThrows(JastacryException.class, () -> {
+            tool.mockupInputOutputEncStreams(layer);
+        });
+    }
+
+    /**
+     * Testcase testDecStream Exceptions.
+     *
+     * @throws JastacryException
+     *             in case of error
+     * @throws IOException will be thrown in test
+     */
+    @Test
+    public void testDecStreamException() throws JastacryException, IOException
+    {
+        Tooling tool = new Tooling();
+        Assertions.assertThrows(JastacryException.class, () -> {
+            tool.mockupInputOutputDecStreams(layer);
+        });
+    }
+
+    /**
+     * Testcase testToString.
+     */
+    @Test
+    public void testToString()
+    {
+        assertEquals("Layer name mismatch", AppendLayer.LAYERNAME, layer.toString());
+    }
+
+    /**
+     * Testcase equals.
+     */
+    @Test
+    public void testEquals()
+    {
+        AppendLayer l1 = new AppendLayer();
+        AppendLayer l2 = new AppendLayer();
+        l1.init(INITVALUE);
+        l2.init(INITVALUE);
+        assertEquals("Layer object equal", l1, l2);
+    }
+
+    /**
+     * Testcase equals.
+     */
+    @Test
+    public void testEqualsSame()
+    {
+        AppendLayer l1 = new AppendLayer();
+        l1.init(INITVALUE);
+        assertEquals("Layer object same", l1, l1);
+    }
+
+    /**
+     * Testcase equals.
+     */
+    @Test
+    public void testNotEqualsNull()
+    {
+        AppendLayer l1 = new AppendLayer();
+        l1.init(INITVALUE);
+        Object o = null;
+        assertEquals("Layer object null unequal", l1.equals(o), false);
+    }
+
+    /**
+     * Testcase equals.
+     */
+    @Test
+    public void testNotEqualsWrongclass()
+    {
+        AppendLayer l1 = new AppendLayer();
+        l1.init(INITVALUE);
+        Object o = new Object();
+        assertEquals("Layer object wrong class unequal", l1.equals(o), false);
+    }
+
+    /**
+     * Testcase hashcode.
+     */
+    @Test
+    public void testHashcode()
+    {
+        AppendLayer l1 = new AppendLayer();
+        AppendLayer l2 = new AppendLayer();
+        l1.init(INITVALUE);
+        l2.init(INITVALUE);
+        assertEquals("Layer hash equal", l1.hashCode(), l2.hashCode());
+    }
+}
