215c11e0b08443f93b77e8586f1343bcc864458c
Michael Kretschmann <kaikretschmann@gmail.com>
2022-07-21 12:59:53 +0000

    Update Worker.java

--- src/main/java/org/jastacry/Worker.java
+++ src/main/java/org/jastacry/Worker.java
@@ -24,8 +24,6 @@ import java.util.concurrent.ThreadPoolExecutor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import org.jastacry.JastacryRuntimeException;
-
 import org.jastacry.GlobalData.Action;
 import org.jastacry.GlobalData.Returncode;
 import org.jastacry.layer.AbstractBasicLayer;
